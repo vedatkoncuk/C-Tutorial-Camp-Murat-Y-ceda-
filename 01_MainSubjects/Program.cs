f0640646f3442d68cb32a7cbef88280f04b6a203
Vedat <vedat.arpa.72@gmail.com>
2026-03-19 16:08:17 +0300

    CSharpTutorial

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region YazdırmaKomutları 
+            //Console.WriteLine("Hello World");
+            //Console.Write("hello");
+
+            //Console.WriteLine("***** Meal Categories *****");
+            //Console.WriteLine();
+
+            //Console.WriteLine("1-Soups");
+            //Console.WriteLine("main meal");
+            //Console.WriteLine("cold starters");
+            //Console.WriteLine("***** Food Categories *****");
+            #endregion
+
+
+            #region String Değişkenler 
+            ////string
+            ////değişken_türü değişken_adı;
+
+            //string name;
+            //name = "Vedat";
+            //Console.WriteLine(name);
+
+            //string customerName;
+            //string customerSurname;
+            //string customerEmail;
+            //string customerPhone;
+            //string customerAdress;
+
+            //customerName ="Ali";
+            //customerSurname = "Çınar";
+            //customerPhone = "+90 400 456 20 10";
+            //customerEmail = "deneme@gmail.com";
+            //customerAdress = "Sakarya";
+
+
+            //Console.WriteLine("****** Rezervasyon Kartı *******");
+
+            //Console.WriteLine();
+            //Console.WriteLine("----------------------------------------");
+            //Console.WriteLine("Müşteri: " + customerName);
+            //Console.WriteLine("İletişim: " + customerPhone);
+            //Console.WriteLine("Email Adresi: " + customerEmail);
+            //Console.WriteLine("Adres: " + customerAdress);
+            //Console.WriteLine("-----------------------------------------");
+
+            #endregion
+
+
+            #region Int Değişkenler
+            //int
+            //int number = 34;
+            //Console.WriteLine(number);
+
+            //int hamburgerPrice = 300;
+            //int cokePrice = 43;
+            //int lemonadePrice = 43;
+            //int pizzaPrice = 500;
+            //int friesPrice = 25;
+            //int waterPrice = 15;
+
+
+            //Console.WriteLine("******* Restoran Menü Fiyatı ******");
+            //Console.WriteLine();
+
+            //Console.WriteLine("-----Hamburger: " + hamburgerPrice + "Tl");
+            //Console.WriteLine("-----Kızartma: " + friesPrice + "Tl");
+            //Console.WriteLine("-----Su: " + waterPrice + "Tl");
+            //Console.WriteLine("-----limonata: " + lemonadePrice + "Tl");
+            //Console.WriteLine("-----Pizza: " + pizzaPrice + "Tl");
+            //Console.WriteLine();
+
+            //int hamburgerCount;
+            //int cokeCount;
+            //int lemonadeCount;
+            //int pizzaCount;
+            //int friesCount;
+            //int waterCount;
+
+            //int totalHamburgerPrice = 0;
+            //int totalCokePrice = 0;
+            //int totalWaterPrice = 0;
+            //int totalPizzaPrice = 0;
+
+            //hamburgerCount = 3;
+            //cokeCount = 3;
+            //lemonadeCount = 3;
+            //pizzaCount = 1;
+            //friesCount = 1;
+            //waterCount = 0;
+
+            //totalHamburgerPrice = hamburgerCount * hamburgerPrice;
+
+            //Console.WriteLine("---------------------------------");
+            //Console.WriteLine("Hamburger Fiyatı: " + totalHamburgerPrice);
+
+            #endregion
+
+
+        }
+    }
+}
+
+
+
+
---
+++ 02-Variables/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Double Değişkenler
+            //double number;
+
+            //number = 3.55;
+            //Console.WriteLine(number);
+
+
+            //Console.WriteLine("***** Fiyat Listesi *****");
+            //Console.WriteLine();
+            //double applePrice, orangePrice, strawberryPrice, potato;
+
+            //applePrice = 24.66;
+            //orangePrice = 12.44;
+            //potato = 13.40;
+            //strawberryPrice = 35.87;
+
+            //Console.WriteLine("----Elma Birim Fiyati:" + applePrice + "TL");
+            //Console.WriteLine("----Portakal Birim Fiyati:" + orangePrice + "TL");
+            //Console.WriteLine("----Çilek Birim Fiyati:" + strawberryPrice + "TL");
+            //Console.WriteLine("----Patates Birim Fiyati:" + potato + "TL");
+            //Console.WriteLine("\n");
+
+            //double appleGram, orangeGram, strawberryGram, potatoGram;
+
+            //appleGram = 1.325;
+            //orangeGram = 3.56;
+            //strawberryGram = 0.750;
+            //potatoGram = 4.850;
+
+            //double appleTotalPrice = appleGram * applePrice;
+            //double potetoTotalPrice = potatoGram * potato;
+            //double strawberryTotalPrice = strawberryGram * strawberryPrice;
+            //double orangeTotalPrice = orangeGram * orangePrice;
+
+            //Console.WriteLine("Elmanın Toplam Fiyatı: " + appleTotalPrice);
+            //Console.WriteLine("Portakal Toplam Fiyatı: " + orangeTotalPrice);
+            //Console.WriteLine("Çilek Toplam Fiyatı: " + strawberryTotalPrice);
+            //Console.WriteLine("Patates Toplam Fiyatı: " + potetoTotalPrice);
+
+            //double shopingTotalPrice = appleTotalPrice + orangeTotalPrice + potetoTotalPrice + strawberryTotalPrice;
+            //Console.WriteLine("\n");
+
+            //Console.WriteLine("Alışveriş Toplam Tutar: " + shopingTotalPrice + "TL");
+
+            #endregion
+
+            #region Klavyeden Veri Girişleri String Değişkenler
+
+            Console.WriteLine("***** ");
+
+            #endregion
+
+
+        }
+    }
+}

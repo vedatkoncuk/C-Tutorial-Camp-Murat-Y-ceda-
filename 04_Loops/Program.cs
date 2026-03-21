3522c4b60cabab9b0ad7e24af25b9336e3b4094d
Vedat <vedat.arpa.72@gmail.com>
2026-03-21 21:13:39 +0300

    lessons 4-5

--- 04_Loops/Program.cs
+++ 04_Loops/Program.cs
@@ -10,7 +10,164 @@ namespace _04_Loops
     {
         static void Main(string[] args)
         {
+            #region For Döngüsü
 
+            //Döngüler 
+
+
+            //int i;
+
+            //for(i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine("C# Eğitim Kampı");
+            //}
+
+            //for (i = 0; i < 20; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (i = 1; i <= 50; i += 3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.Write("Lütfen ekrana yazılamasınıı istediğiniz adedi giriniz: ");
+            //int finishValue = int.Parse(Console.ReadLine());
+
+            //for (i = 1; i <= finishValue; i++)
+            //{
+            //    Console.WriteLine("girilen aded sayısı kadar prompt");
+            //}
+
+            #endregion
+
+
+            #region For döngüsü ile karar yapıları 
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //   if(i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+            //int totalValue = 0;
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //  totalValue += i;
+            //}
+
+            //Console.WriteLine(totalValue);
+
+
+            //int totalValue = 0;
+
+            //for (int i = 1; i <= 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        totalValue += i;
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //Console.WriteLine("--------");
+            //Console.WriteLine(totalValue);
+
+
+            //int count = 0;
+
+            //for (int i = 1; i <= 50; i++)
+            //{
+            //    if (i % 7 == 0)
+            //    {
+            //     count++;
+            //    }
+            //}
+
+            //Console.WriteLine(count);
+
+
+
+            //int bacterium = 1;
+
+            //for(int i = 1; i <= 24; i++)
+            //{
+            //     bacterium *= 2;
+
+            //    Console.WriteLine(bacterium);
+            //}
+
+
+
+
+            #endregion
+
+
+            #region While Döngüsü
+
+            //int i = 1;
+
+            //while (i <= 5)
+            //{
+            //    Console.WriteLine("C# Eğitim Kampı");
+            //    i++;
+            //}
+
+            //int i = 1;
+
+            //while(i <= 10)
+            //{
+            //    if(i % 3 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //    i++;
+            //}
+
+
+            //int i = 1;
+            //int sum = 0;
+
+            //while(i <= 10)
+            //{
+            //    sum += i;
+            //    i++;
+            //}
+
+            //Console.WriteLine(sum);
+
+            #endregion
+
+
+            #region Örnek Sınav Sorusu  
+
+            //klavyeden girilen 3 basamaklı bir sayının basamaklarının toplamını ekrana yazdıran programı yazınız.
+
+          //  int ones, tens, hundreds, sum, userValue;
+
+          //  Console.WriteLine("Lütfen basamaklarını toplamak istediğiniz sayıyı giriniz: ");
+
+          //  userValue = int.Parse(Console.ReadLine());
+
+          //ones = userValue % 10;
+          //tens = (userValue % 100) / 10;
+          //hundreds = userValue / 100;
+
+          //  sum = ones + tens + hundreds;
+
+          //  Console.WriteLine("birler : " + ones);
+          //  Console.WriteLine("Onlar : " + tens);
+          //  Console.WriteLine("Yüzler: " + hundreds);
+
+          //  Console.WriteLine(sum);
+
+            #endregion
         }
     }
 }
---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Alt alta yıldız basma
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            #endregion
+
+
+            #region Alt alta yıldız basma her satırda 10 yıldız
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine("************");
+            //}
+
+            #endregion
+
+
+            #region Dik üçgen şeklinde yıldız basma
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+
+            #region Ters dik üçgen şeklinde yıldız basma
+
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+
+            #region Dik üçgen ve ters dik üçgen şeklinde yıldız basma
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    if(i <= 5)
+            //    {
+            //        for (int j = 1; j <= i; j++)
+            //        {
+            //            Console.Write("*");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        for (int k = 10; k > i; k--)
+            //        {
+            //            Console.Write("*");
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+
+            #region Baklava dilimi şeklinde yıldız basma
+
+            //int n = 5;
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 1; j <= n - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}                   
+
+            #endregion
+
+
+            #region Piramit şeklinde yıldız basma
+
+            //int n = 5;
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 1; j <= n - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}                   
+
+            #endregion
+
+
+
+            #region Ters Piramit 
+
+            //int n = 5;
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 1; j <= n - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}                   
+
+            #endregion
+
+
+
+        }
+    }
+}
